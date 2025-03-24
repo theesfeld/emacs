d51499d145005fb97dce7516aeae2b608bfde73a
theesfeld <william@theesfeld.net>
2025-03-24 15:53:53 -0400

    ATIC

--- init.el
+++ init.el
@@ -1170,13 +1170,7 @@
  :after (all-the-icons marginalia corfu)
  :config (all-the-icons-completion-mode)
  :hook
- ((all-the-icons-completion-mode
-   .
-   (lambda ()
-     (when (featurep 'corfu)
-       (setq corfu-margin-formatters
-             '(all-the-icons-completion-margin-formatter)))))
-  (marginalia-mode . all-the-icons-completion-marginalia-setup)))
+ (marginalia-mode . all-the-icons-completion-marginalia-setup))
 
 (use-package
  alert
