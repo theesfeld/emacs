60739bcc3824acae20af4fba47eb5a2029cfcbad
theesfeld <william@theesfeld.net>
2025-03-24 13:46:43 -0400

    aidermacs models

--- init.el
+++ init.el
@@ -1694,11 +1694,11 @@
  aidermacs
  :ensure t
  :config
- (setq aidermacs-default-model "anthropic/claude-3-7-sonnet-20250219")
+ (setq aidermacs-default-model "sonnet")
  (global-set-key (kbd "C-c A") 'aidermacs-transient-menu)
  (setq aidermacs-auto-commits t)
  (setq aidermacs-use-architect-mode t)
- (setq aidermacs-architect-model "o1-mini")
+ (setq aidermacs-architect-model "sonnet")
  (setq aidermacs-editor-model "deepseek/deepseek-chat"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
