0296c96438a633024d50c05eb23c40830d80e4a6
theesfeld <william@theesfeld.net>
2025-04-10 15:40:28 -0400

    asdasd

--- init.el
+++ init.el
@@ -565,22 +565,22 @@
             (exwm-workspace-move-window i))))
        (number-sequence 0 9)))))
 
-   ;; Simulation Keys
-   (setq exwm-input-simulation-keys
-         '(([?\C-b] . [left])
-           ([?\C-f] . [right])
-           ([?\C-p] . [up])
-           ([?\C-n] . [down])
-           ([?\C-a] . [home])
-           ([?\C-e] . [end])
-           ([?\M-v] . [prior])
-           ([?\C-v] . [next])
-           ([?\C-d] . [delete])
-           ([?\C-k] . [S-end delete])
-           ([?\M-w] . [?\C-c])
-           ([?\C-y] . [?\C-v])))
+  ;; Simulation Keys
+  (setq exwm-input-simulation-keys
+        '(([?\C-b] . [left])
+          ([?\C-f] . [right])
+          ([?\C-p] . [up])
+          ([?\C-n] . [down])
+          ([?\C-a] . [home])
+          ([?\C-e] . [end])
+          ([?\M-v] . [prior])
+          ([?\C-v] . [next])
+          ([?\C-d] . [delete])
+          ([?\C-k] . [S-end delete])
+          ([?\M-w] . [?\C-c])
+          ([?\C-y] . [?\C-v])))
 
-   (exwm-enable))
+  (exwm-enable)
 
   (use-package
    exwm-edit
