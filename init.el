5598508a7af2c0e72b2668d5676fc48387dc8df1
theesfeld <william@theesfeld.net>
2025-04-15 21:50:27 -0400

    a

--- init.el
+++ init.el
@@ -1970,73 +1970,63 @@
 ;;                                  DENOTE                                   ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package
- denote
- :ensure t
- ; defer
- :defer t
- :hook
- ( ;; If you use Markdown or plain text files, then you want to make
-  ;; the Denote links clickable (Org renders links as buttons right
-  ;; away)
-  (text-mode . denote-fontify-links-mode-maybe)
-  ;; Apply colours to Denote names in Dired.  This applies to all
-  ;; directories.  Check `denote-dired-directories' for the specific
-  ;; directories you may prefer instead.  Then, instead of
-  ;; `denote-dired-mode', use `denote-dired-mode-in-directories'.
-  (dired-mode . denote-dired-mode))
- :bind
- ;; Denote DOES NOT define any key bindings.  This is for the user to
- ;; decide.  For example:
- (:map
-  global-map
-  ("C-c n n" . denote)
-  ("C-c n d" . denote-sort-dired)
-  ("C-c n S" . denote-search)
-  ;; If you intend to use Denote with a variety of file types, it is
-  ;; easier to bind the link-related commands to the `global-map', as
-  ;; shown here.  Otherwise follow the same pattern for `org-mode-map',
-  ;; `markdown-mode-map', and/or `text-mode-map'.
-  ("C-c n l" . denote-link)
-  ("C-c n L" . denote-add-links)
-  ("C-c n b" . denote-backlinks)
-  ;; Note that `denote-rename-file' can work from any context, not just
-  ;; Dired bufffers.  That is why we bind it here to the `global-map'.
-  ("C-c n r" . denote-rename-file)
-  ("C-c n R" . denote-rename-file-using-front-matter)
+(use-package denote
+  :ensure t
+  :hook
+  ( ;; If you use Markdown or plain text files, then you want to make
+   ;; the Denote links clickable (Org renders links as buttons right
+   ;; away)
+   (text-mode . denote-fontify-links-mode-maybe)
+   ;; Apply colours to Denote names in Dired.  This applies to all
+   ;; directories.  Check `denote-dired-directories' for the specific
+   ;; directories you may prefer instead.  Then, instead of
+   ;; `denote-dired-mode', use `denote-dired-mode-in-directories'.
+   (dired-mode . denote-dired-mode))
+  :bind
+  ;; Denote DOES NOT define any key bindings.  This is for the user to
+  ;; decide.  For example:
+  ( :map global-map
+    ("C-c n n" . denote)
+    ("C-c n d" . denote-dired)
+    ;; ("C-c n g" . denote-grep)
+    ;; If you intend to use Denote with a variety of file types, it is
+    ;; easier to bind the link-related commands to the `global-map', as
+    ;; shown here.  Otherwise follow the same pattern for `org-mode-map',
+    ;; `markdown-mode-map', and/or `text-mode-map'.
+    ("C-c n l" . denote-link)
+    ("C-c n L" . denote-add-links)
+    ("C-c n b" . denote-backlinks)
+    ("C-c n q c" . denote-query-contents-link) ; create link that triggers a grep
+    ("C-c n q f" . denote-query-filenames-link) ; create link that triggers a dired
+    ;; Note that `denote-rename-file' can work from any context, not just
+    ;; Dired bufffers.  That is why we bind it here to the `global-map'.
+    ("C-c n r" . denote-rename-file)
+    ("C-c n R" . denote-rename-file-using-front-matter)
 
-  ;; Key bindings specifically for Dired.
-  :map
-  dired-mode-map
-  ("C-c C-d C-i" . denote-dired-link-marked-notes)
-  ("C-c C-d C-r" . denote-dired-rename-files)
-  ("C-c C-d C-k" . denote-dired-rename-marked-files-with-keywords)
-  ("C-c C-d C-R"
-   .
-   denote-dired-rename-marked-files-using-front-matter))
+    ;; Key bindings specifically for Dired.
+    :map dired-mode-map
+    ("C-c C-d C-i" . denote-dired-link-marked-notes)
+    ("C-c C-d C-r" . denote-dired-rename-files)
+    ("C-c C-d C-k" . denote-dired-rename-marked-files-with-keywords)
+    ("C-c C-d C-R" . denote-dired-rename-marked-files-using-front-matter))
 
- :config
- ;; Remember to check the doc string of each of those variables.
- (setq denote-directory (expand-file-name "~/.org/notes"))
- (setq denote-save-buffers nil)
- (setq denote-known-keywords '("emacs" "research"))
- (setq denote-infer-keywords t)
- (setq denote-sort-keywords t)
- (setq denote-prompts '(title keywords))
- (setq denote-excluded-directories-regexp nil)
- (setq denote-excluded-keywords-regexp nil)
- (setq denote-rename-confirmations
-       '(rewrite-front-matter modify-file-name))
+  :config
+  ;; Remember to check the doc string of each of those variables.
+  (setq denote-directory (expand-file-name "~/Documents/notes/"))
+  (setq denote-save-buffers nil)
+  (setq denote-known-keywords '("emacs" "philosophy" "politics" "economics"))
+  (setq denote-infer-keywords t)
+  (setq denote-sort-keywords t)
+  (setq denote-prompts '(title keywords))
+  (setq denote-excluded-directories-regexp nil)
+  (setq denote-excluded-keywords-regexp nil)
+  (setq denote-rename-confirmations '(rewrite-front-matter modify-file-name))
 
- ;; Pick dates, where relevant, with Org's advanced interface:
- (setq denote-date-prompt-use-org-read-date t)
+  ;; Pick dates, where relevant, with Org's advanced interface:
+  (setq denote-date-prompt-use-org-read-date t)
 
- ;; By default, we do not show the context of links.  We just display
- ;; file names.  This provides a more informative view.
- (setq denote-backlinks-show-context t)
-
- ;; Automatically rename Denote buffers using the `denote-rename-buffer-format'.
- (denote-rename-buffer-mode 1))
+  ;; Automatically rename Denote buffers using the `denote-rename-buffer-format'.
+  (denote-rename-buffer-mode 1))
 
 (use-package consult-denote
   :ensure t
@@ -2087,30 +2077,6 @@
  ;;   denote-org-dblock-insert-missing-links
  ;;   denote-org-dblock-insert-files-as-headings))
  )
-
-(use-package denote-sequence
-  :ensure t
-  :bind
-  ( :map global-map
-    ;; Here we make "C-c n s" a prefix for all "[n]otes with [s]equence".
-    ;; This is just for demonstration purposes: use the key bindings
-    ;; that work for you.  Also check the commands:
-    ;;
-    ;; - `denote-sequence-new-parent'
-    ;; - `denote-sequence-new-sibling'
-    ;; - `denote-sequence-new-child'
-    ;; - `denote-sequence-new-child-of-current'
-    ;; - `denote-sequence-new-sibling-of-current'
-    ("C-c n s s" . denote-sequence)
-    ("C-c n s f" . denote-sequence-find)
-    ("C-c n s l" . denote-sequence-link)
-    ("C-c n s d" . denote-sequence-dired)
-    ("C-c n s r" . denote-sequence-reparent)
-    ("C-c n s c" . denote-sequence-convert))
-  :config
-  ;; The default sequence scheme is `numeric'.
-  (setq denote-sequence-scheme 'alphanumeric))
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                              ERC (IRC Client)                             ;;
