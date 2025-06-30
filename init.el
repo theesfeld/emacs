ef8162fed6da263ddbab52348e8edf6019ff3c45
theesfeld <william@theesfeld.net>
2025-06-29 22:04:38 -0400

    aaaa

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;;; init.el -*- lexical-binding: t -*-
 
-;; Time-stamp: <Last changed 2025-06-29 21:37:17 by grim>
+;; Time-stamp: <Last changed 2025-06-29 22:04:36 by grim>
 
 ;; Enable these
 (mapc
@@ -3487,8 +3487,42 @@ parameters set in early-init.el to ensure robust UI element disabling."
   (insert-uuid-default-version 4)
   (insert-uuid-uppercase nil))
 
+;;; beframe
+
+(use-package beframe
+  :ensure t
+  :bind(("C-c b" . beframe-prefix-map))
+  :config
+  (setq beframe-global-buffers '("*scratch*" "*Messages*" "*Backtrace*"))
+  (beframe-mode 1)
+  (defvar consult-buffer-sources)
+  (declare-function consult--buffer-state "consult")
+
+  (with-eval-after-load 'consult
+    (defface beframe-buffer
+      '((t :inherit font-lock-string-face))
+      "Face for `consult' framed buffers.")
+
+    (defun my-beframe-buffer-names-sorted (&optional frame)
+      "Return the list of buffers from `beframe-buffer-names' sorted by visibility.
+With optional argument FRAME, return the list of buffers of FRAME."
+      (beframe-buffer-names frame :sort #'beframe-buffer-sort-visibility))
+
+    (defvar beframe-consult-source
+      `( :name     "Frame-specific buffers (current frame)"
+         :narrow   ?F
+         :category buffer
+         :face     beframe-buffer
+         :history  beframe-history
+         :items    ,#'my-beframe-buffer-names-sorted
+         :action   ,#'switch-to-buffer
+         :state    ,#'consult--buffer-state))
+
+    (add-to-list 'consult-buffer-sources 'beframe-consult-source)))
+
 ;;; final cleanup
 
 (put 'eshell 'disabled nil)
 (provide 'init)
+
 ;;; init.el ends here
