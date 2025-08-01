f4e37a9d13c28e3a8457811f73a3dc0b6e208c9d
theesfeld <william@theesfeld.net>
2025-08-01 17:12:03 -0400

    derp

--- init.el
+++ init.el
@@ -18,7 +18,6 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ;;; Commentary:
-;; Email: tj@emacs.su
 ;;; Code:
 (defun my/get-system-memory ()
   "Get system memory in GB."
